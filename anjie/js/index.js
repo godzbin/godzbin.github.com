ca09bc4052e37cfe1e73933eaac4b0145d72c2e4
unknown <>
2016-10-13 22:44:27 +0800

    幻灯片的滚动停止

--- anjie/js/index.js
+++ anjie/js/index.js
@@ -1,31 +1,34 @@
-$(document).ready(function () {
+$(document).ready(function() {
     main.init();
     main.textareaPlaceholder();
     main.initSlide();
-    setInterval(function () {
+
+    
+    setInterval(function() {
         main.nextSlide();
     }, 5000);
 
-    new selectComponent("startProvince", provinceListMain, function (){
+    new selectComponent("startProvince", provinceListMain, function() {
         var cityId = "startCity";
         var province = $("#startProvince").val();
         selectComponentChange(cityId, province);
     }).init();
-    new selectComponent("endProvince", provinceListMain, function(){
+    new selectComponent("endProvince", provinceListMain, function() {
         selectComponentChange("endCity", $("#endProvince").val());
     }).init();
-    new selectComponent("networkProvince", provinceListMain, function(){
+    new selectComponent("networkProvince", provinceListMain, function() {
         selectComponentChange("networkCity", $("#endProvince").val());
     }).init();
 });
 var main = {
     isLoadAmount: false,
-    init: function () {
+    isRunning: false,
+    init: function() {
         main.initNewTab();
         main.initScroll();
     },
-    initNewTab: function () {
-        $(".new-right-tab-header-box").click(function () {
+    initNewTab: function() {
+        $(".new-right-tab-header-box").click(function() {
             if (!$(this).hasClass("action")) {
                 $(".new-right-tab-header-box").removeClass("action");
                 $(this).addClass("action");
@@ -39,8 +42,8 @@ var main = {
             }
         });
     },
-    initScroll: function () {
-        $(document).scroll(function () {
+    initScroll: function() {
+        $(document).scroll(function() {
             var mainTop = $(document).scrollTop();
             var newTop = $(".new").offset().top;
             if (mainTop + 300 > newTop && !main.isLoadAmount) {
@@ -48,7 +51,7 @@ var main = {
             }
         });
     },
-    setAmount: function () {
+    setAmount: function() {
         main.isLoadAmount = true;
         var values = $(".amount-main-box-value");
         var length = values.length;
@@ -63,30 +66,36 @@ var main = {
             var rate = value / max;
             var height = maxHeight * rate;
             // $(values[i]).height(height);
-            $(values[i]).animate({height: height}, 1000);
+            $(values[i]).animate({
+                height: height
+            }, 1000);
         }
     },
     //多行文本框的提示语显示
-    textareaPlaceholder: function () {
+    textareaPlaceholder: function() {
         var textarea = $(".model-table-form-textarea");
         var placeholder = $(".model-table-form-textarea-placeholder");
-        textarea.focus(function () {
+        textarea.focus(function() {
             placeholder.hide();
         });
-        textarea.blur(function () {
+        textarea.blur(function() {
             if (textarea.val() === "") {
                 placeholder.show();
             }
         });
     },
-    getSlideBoxs: function () {
+    getSlideBoxs: function() {
         return $(".slide-img-box");
     },
-    initSlide: function () {
+    initSlide: function() {
         $(".slide-ctrl-before").click(this.beforeSlide);
         $(".slide-ctrl-next").click(this.nextSlide);
     },
-    beforeSlide: function () {
+    beforeSlide: function() {
+        if (main.isRunning) {
+            return;
+        }
+        main.isRunning = true;
         var boxs = main.getSlideBoxs();
         var length = boxs.length;
         for (var i = 0; i < length; i++) {
@@ -94,36 +103,61 @@ var main = {
             if (box.hasClass("action")) {
                 box.removeClass("action");
                 box.css("left", "0");
-                box.animate({left: "100%"}, 1000);
+                box.animate({
+                    left: "100%"
+                }, 1000);
                 var prevBox = box.prev();
                 if (!prevBox.length || prevBox.hasClass("slide-img-model")) {
                     prevBox = $(boxs[length - 1]);
                 }
                 prevBox.css("left", "-100%");
+
+
+
                 if (prevBox) {
-                    prevBox.animate({"left": "0"}, 1000);
+                    prevBox.animate({
+                        "left": "0"
+                    }, 1000, function() {
+                        main.isRunning = false;
+                    });
                     prevBox.addClass("action");
                 }
                 return;
             }
         }
     },
-    nextSlide: function () {
+    nextSlide: function() {
+        if (main.isRunning) {
+            return;
+        }
+        main.isRunning = true;
         var boxs = main.getSlideBoxs();
         var length = boxs.length;
         for (var i = 0; i < length; i++) {
             var box = $(boxs[i]);
             if (box.hasClass("action")) {
+
+
+
                 box.removeClass("action");
                 box.css("left", "0");
-                box.animate({left: "-100%"}, 1000);
+                box.animate({
+                    left: "-100%"
+                }, 1000);
                 var nextBox = box.next();
                 if (!nextBox.length) {
                     nextBox = $(boxs[0]);
                 }
                 nextBox.css("left", "100%");
+
+
+
                 if (nextBox) {
-                    nextBox.animate({"left": "0"}, 1000);
+                    nextBox.animate({
+                        "left": "0"
+                    }, 1000, function() {
+                        main.isRunning = false;
+                    });
                     nextBox.addClass("action");
                 }
                 return;
@@ -131,26 +165,27 @@ var main = {
         }
     }
 };
+
 function selectComponent(id, data, changeFn) {
     this.id = id || "";
     this.data = data || [];
     this.changeFn = changeFn || new Function();
-    this.getSelect = function () {
+    this.getSelect = function() {
         return $("#" + id);
     };
-    this.init = function () {
+    this.init = function() {
         var selectEl = this.getSelect();
         var length = this.data.length;
-        var firstOption =  selectEl.find("option:first-child");
+        var firstOption = selectEl.find("option:first-child");
         selectEl.html("");
         selectEl.append(firstOption);
         for (var i = 0; i < length; i++) {
-            var option = this.createOption(data[i]["name"] || data[i],data[i]["name"] || data[i]);
+            var option = this.createOption(data[i]["name"] || data[i], data[i]["name"] || data[i]);
             selectEl.append(option);
         }
         selectEl.change(this.changeFn);
     };
-    this.createOption = function (name, value) {
+    this.createOption = function(name, value) {
         var optionEl = $("<option></option>");
         optionEl.attr("value", value);
         optionEl.text(name);
@@ -158,17 +193,18 @@ function selectComponent(id, data, changeFn) {
     };
     return this;
 }
-function selectComponentChange(cityId, province){
+
+function selectComponentChange(cityId, province) {
     var i = 0;
-    while (provinceListMain[i]){
-        if(provinceListMain[i]["name"] == province ){
+    while (provinceListMain[i]) {
+        if (provinceListMain[i]["name"] == province) {
             var cityList = [];
-            if(province == "北京" || province == "天津" || province == "上海" || province == "重庆"){
+            if (province == "北京" || province == "天津" || province == "上海" || province == "重庆") {
                 cityList = provinceListMain[i].cityList[0].areaList.concat(provinceListMain[i].cityList[1].areaList);
-            }else{
-                cityList =  provinceListMain[i]["cityList"];
+            } else {
+                cityList = provinceListMain[i]["cityList"];
             }
-            new selectComponent(cityId,cityList).init();
+            new selectComponent(cityId, cityList).init();
             return;
         }
         i++;
