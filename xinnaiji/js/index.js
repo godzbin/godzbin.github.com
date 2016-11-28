fbd34a68fcd5c9a059b82f8c61b6ef10af7f8d46
zbchen <zbchen@allpaycloud.cn>
2016-11-28 14:40:59 +0800

    commit

--- xinnaiji/js/index.js
+++ xinnaiji/js/index.js
@@ -883,7 +883,7 @@
 					aboutContent.innerHTML = data.toString();
 					function getPicInfo() {
 						var imgObj = document.getElementsByTagName('img'); //获取图文中所有的img标签对象
-
+						var imgs = [];
 						for (var i = 0; i < imgObj.length; i++) {
 							imgs.push(imgObj[i].src);
 							nowImgurl = this.src; //获取当前点击图片url
